678e6e6f40f41ddc55f5c1cde4ea87e267f0ab21
LJDJGC <singsongsmusic@gmail.com>
2026-01-30 06:16:03 +0900

    docs: Initialize JavaScript practice playground

--- playground/script.js
+++ playground/script.js
@@ -1,5 +1,31 @@
-const variable = 'property';
+function Person(name) {
+  this.name = name;
+}
 
-myObject.variable; // this gives us 'undefined' because it's looking for a property named 'variable' in our object
+Person.prototype.sayName = function() {
+  console.log(`Hello, I'm ${this.name}!`);
+};
 
-myObject[variable]; // this is equivalent to myObject['property'] and returns 'Value!'
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+}
+
+Player.prototype.getMarker = function() {
+  console.log(`My marker is '${this.marker}'`);
+};
+
+Object.getPrototypeOf(Player.prototype); // returns Object.prototype
+
+// Now make `Player` objects inherit from `Person`
+Object.setPrototypeOf(Player.prototype, Person.prototype);
+Object.getPrototypeOf(Player.prototype); // returns Person.prototype
+
+const player1 = new Player('yohe', 'X');
+const player2 = new Player('tanaka kakuei', 'O');
+
+player1.sayName(); 
+player2.sayName(); 
+
+player1.getMarker(); // My marker is 'X'
+player2.getMarker(); // My marker is 'O'
