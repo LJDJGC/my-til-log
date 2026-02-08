c94b9db83df80f6df5ce15ea91197ef193c10bc5
LJDJGC <singsongsmusic@gmail.com>
2026-02-08 10:06:40 +0900

    feat(playground): experiment with factory functions and closures

--- playground/script.js
+++ playground/script.js
@@ -1,23 +1,10 @@
-let hamster = {
-  stomach: [],
+const age = 100;
 
-  eat(food) {
-    // this.stomach.push の代わりに this.stomach に代入する
-    this.stomach = [food];
-  }
-};
+function go () { 
+  const age = 200;
+  const hair = 'blonde';
+  console.log(hair);
+  console.log(age);
+}
 
-let speedy = {
-   __proto__: hamster
-};
-
-let lazy = {
-  __proto__: hamster
-};
-
-// Speedy は食べ物を見つけました
-speedy.eat("apple");
-alert( speedy.stomach ); // apple
-
-// Lazy の胃は空っぽです
-alert( lazy.stomach ); // <nothing>
+console.log(age);
