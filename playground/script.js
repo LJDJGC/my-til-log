b7b582c68d6265b141cd04e9b05c6afa7f417a95
LJDJGC <singsongsmusic@gmail.com>
2026-02-12 05:50:27 +0900

    chore(playground): add closure practice code from Wes Bos

--- playground/script.js
+++ playground/script.js
@@ -1,9 +1,11 @@
-  function outer(){
-    const outerVar = 'Hey I am the outer Var';
+function  createGame(gameName){
+  let score = 0;
 
-    function inner(){
-      const innerVar = "hey I am an inner var";
-      console.log(innerVar);
-      console.log(outerVar);
-    }
+  return function win(){
+    score ++;
+    return `Your name ${gameName} score is ${score}`
   }
+}
+
+const hockeyGame = createGame('Hockey');
+const sccerGame = createGame('Soccer');
