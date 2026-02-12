e0dc6e1d82ffc681762335d58a7669212bb8e67c
LJDJGC <singsongsmusic@gmail.com>
2026-02-13 06:32:36 +0900

    feat: implement basic module pattern using IIFE

--- playground/script.js
+++ playground/script.js
@@ -1,23 +1,6 @@
-const documentMock = (() => ({
-  querySelector: (selector) => ({
-    innerHTML: null,
-  }),
-}))();
-
-const Formatter = (function(doc) {
-  const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-
-  const makeUppercase = (text) => {
-    log("Making uppercase");
-    return text.toUpperCase();
-  };
-
-  const writeToDOM = (selector, message) => {
-    doc.querySelector(selector).innerHTML = message;
-  }
-
-  return {
-    makeUppercase,
-    writeToDOM,
-  }
-})(document || documentMock);
+if (Math.random() > 0.5) {
+  var x = 1;
+} else {
+  var x = 2;
+}
+console.log(x);
